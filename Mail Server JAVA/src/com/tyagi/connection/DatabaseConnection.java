b8e286b4305a5f1bbde523a8442ec037cb867b7d
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-08-01 23:12:20 +0530

    Database Connection !

---
+++ Mail Server JAVA/src/com/tyagi/connection/DatabaseConnection.java
@@ -0,0 +1,25 @@
+package com.tyagi.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DatabaseConnection {
+	
+	
+
+	private static String mysql_driverName = "com.mysql.jdbc.Driver";
+	private static String mysql_dbUrl = "jdbc:mysql://localhost:3306/online_food_order";
+	private static String mysql_userName = "root";
+	private static String mysql_dbPass = "";
+
+	
+
+	public static Connection getCon() throws Exception {
+		Class.forName(mysql_driverName);
+		Connection con = DriverManager.getConnection(mysql_dbUrl, mysql_userName, mysql_dbPass);
+		return con;
+	}
+
+}
