e81189912fe948dcc96e825ab09b47f01a46f16b
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-08-14 00:14:46 +0530

    Model !

---
+++ Mail Server JAVA/src/com/model/MyData.java
@@ -0,0 +1,72 @@
+package com.model;
+
+import java.io.Serializable;
+
+public class MyData implements Serializable {
+	
+	private String userid;
+	private String password;
+	private String fname;
+	private String lname;
+	private String dob;
+	private String gender;
+	private String mobile;
+	private String country;
+	
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	@Override
+	public String toString() {
+		return "MyData [userid=" + userid + ", password=" + password + ", fname=" + fname + ", lname=" + lname
+				+ ", dob=" + dob + ", gender=" + gender + ", mobile=" + mobile + ", country=" + country + "]";
+	}
+	
+
+}
